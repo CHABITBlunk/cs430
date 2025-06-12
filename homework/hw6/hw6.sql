5c98805b2c8258c66c76a02d83f747c9780316c4
chabitblunk <cblunk097@gmail.com>
2025-06-11 18:40:42 -0600

    schema for hw6

---
+++ homework/hw6/hw6.sql
@@ -0,0 +1,44 @@
+drop table if exists member, book, publisher, author, phone;
+
+create table member (
+  member_id   int           not null,
+  first_name  varchar(10)   not null,
+  last_name   varchar(10)   not null,
+  dob         date          not null
+  PRIMARY KEY(member_id)
+);
+
+create table book (
+  isbn            int         not null,
+  title           varchar(35) not null,
+  year_published  int         not null
+);
+
+create table publisher (
+  pub_id    int         not null,
+  pub_name  varchar(25) not null
+);
+
+create table phone (
+  p_number    int         not null,
+  phone_type  varchar(20) not null
+);
+
+create table author (
+  author_id   int not null,
+  first_name  varchar(10),
+  last_name   varchar(10)
+);
+
+create table borrowed (
+  member_id       int   not null,
+  isbn            int   not null,
+  checkout_date   date  not null default current_date,
+  checkin_date    date,
+  primary key(member_id, isbn, checkout_date),
+  foreign key(member_id) references member(member_id),
+  foreign key(isbn) references book(isbn)
+);
+
+create or replace sql security invoker view borrowed_by as
+  select isbn, member_id, checkin_date, checkout_date from borrowed;
