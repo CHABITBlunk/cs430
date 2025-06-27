b75e1413e0b8405c7caa9279a3cb4b2e659ab781
chabitblunk <cblunk097@gmail.com>
2025-06-26 20:47:11 -0600

    one last formatting thing

--- homework/hw9/Lab9.java
+++ homework/hw9/Lab9.java
@@ -127,6 +127,8 @@ public class Lab9 {
       readXML("./libdata.xml", stmt);
 
       printResultSet(stmt.executeQuery("select * from borrowed;"));
+      System.out.println();
+      System.out.println();
       printResultSet(stmt.executeQuery(
           "select m.last_name, m.first_name, m.member_id, b.title, br.lib_name from borrowed br join book b on br.isbn = b.isbn join member m on br.member_id = m.member_id where br.checkin_date is null order by m.last_name, m.first_name;"));
 
